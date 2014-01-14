fd840a4f7fb1deb4118d0cad04080093a9ffde0a
Eivind Fjeldstad <eivind.fjeldstad@gmail.com>
2014-01-14 02:24:32 +0100

    use mocha for tests

--- test.js
+++ test.js
@@ -1,23 +1,38 @@
 var assert = require('./');
 var ok = require('assert');
-var error;
 
-try {
-  assert(false, 401, 'fail');
-} catch (err) {
-  ok(err.status == 401);
-  ok(err.message == 'fail');
-  ok(err.expose);
-}
-
-try {
-  assert(false, 401, 'fail', { expose: false });
-} catch (err) {
-  error = err;
-  ok(err.status == 401);
-  ok(err.message == 'fail');
-  ok(!err.expose);
-}
-
-ok(error);
-console.log('All tests passed');
+describe('assert()', function () {
+  it('should throw when guard is falsy', function () {
+    var err;
+    
+    try {
+      assert(false, 401, 'fail');
+    } catch (e) {
+      err = e;
+    }
+    
+    ok(err);
+    ok(err.status == 401);
+    ok(err.message == 'fail');
+    ok(err.expose);
+  })
+  
+  it('should not throw when guard is truthy', function () {
+    assert(true, 401, 'fail');
+  })
+  
+  it('should accept options for the error object', function () {
+    var err;
+    
+    try {
+      assert(false, 401, 'fail', { expose: false });
+    } catch (e) {
+      err = e;
+    }
+    
+    ok(err);
+    ok(err.status == 401);
+    ok(err.message == 'fail');
+    ok(!err.expose);
+  })
+})
