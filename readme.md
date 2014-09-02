c77a83dad8be40deb235ac515860a4b844dd5293
Jonathan Ong <jonathanrichardong@gmail.com>
2014-09-02 01:24:52 -0700

    add coverage and badges

--- readme.md
+++ readme.md
@@ -1,4 +1,12 @@
 #http-assert
+
+[![NPM version][npm-image]][npm-url]
+[![Build status][travis-image]][travis-url]
+[![Test coverage][coveralls-image]][coveralls-url]
+[![Dependency Status][david-image]][david-url]
+[![License][license-image]][license-url]
+[![Downloads][downloads-image]][downloads-url]
+
 Assert with status codes. Like ctx.throw() in Koa, but with a guard.
 
 ##Example
@@ -17,3 +25,16 @@ try {
 
 ##Licence
 MIT
+
+[npm-image]: https://img.shields.io/npm/v/http-assert.svg?style=flat-square
+[npm-url]: https://npmjs.org/package/http-assert
+[travis-image]: https://img.shields.io/travis/jshttp/http-assert.svg?style=flat-square
+[travis-url]: https://travis-ci.org/jshttp/http-assert
+[coveralls-image]: https://img.shields.io/coveralls/jshttp/http-assert.svg?style=flat-square
+[coveralls-url]: https://coveralls.io/r/jshttp/http-assert?branch=master
+[david-image]: http://img.shields.io/david/jshttp/http-assert.svg?style=flat-square
+[david-url]: https://david-dm.org/jshttp/http-assert
+[license-image]: http://img.shields.io/npm/l/http-assert.svg?style=flat-square
+[license-url]: LICENSE
+[downloads-image]: http://img.shields.io/npm/dm/http-assert.svg?style=flat-square
+[downloads-url]: https://npmjs.org/package/http-assert
--- test/test.js
+++ test/test.js
@@ -1,65 +1,65 @@
-var assert = require('./');
+var assert = require('..');
 var ok = require('assert');
 
 describe('assert()', function () {
   it('should throw when guard is falsy', function () {
     var err;
-    
+
     try {
       assert(false, 401, 'fail');
     } catch (e) {
       err = e;
     }
-    
+
     ok(err);
     ok(err.status == 401);
     ok(err.message == 'fail');
     ok(err.expose);
   })
-  
+
   it('should not throw when guard is truthy', function () {
     assert(true, 401, 'fail');
   })
-  
+
   it('should accept options for the error object', function () {
     var err;
-    
+
     try {
       assert(false, 401, 'fail', { expose: false });
     } catch (e) {
       err = e;
     }
-    
+
     ok(err);
     ok(err.status == 401);
     ok(err.message == 'fail');
     ok(!err.expose);
   })
-  
+
   it('should not expose 5xx errors', function () {
     var err;
-    
+
     try {
       assert(false, 500);
     } catch (e) {
       err = e;
     }
-    
+
     ok(err);
     ok(err.status == 500);
     ok(err.message == 'Internal Server Error');
     ok(!err.expose);
   });
-  
+
   it('should default to status code 500', function () {
     var err;
-    
+
     try {
       assert(false, 'fail');
     } catch (e) {
       err = e;
     }
-    
+
     ok(err);
     ok(err.status == 500);
     ok(err.message == 'fail');
