35859bb78a8f35a3d9bb934217fed4600695efcc
dead_horse <dead_horse@qq.com>
2014-09-09 13:56:35 +0800

    use http-errors

--- index.js
+++ index.js
@@ -1,28 +1,6 @@
-var http = require('http');
+var createError = require('http-errors');
 
 module.exports = function (value, status, msg, opts) {
   if (value) return;
-
-  if (typeof status != 'number') {
-    opts = msg;
-    msg = status;
-    status = 500;
-  }
-
-  if (typeof msg != 'string') {
-    opts = msg;
-    msg = http.STATUS_CODES[status];
-  }
-
-  var err = new Error(msg);
-  err.status = status;
-  err.expose = http.STATUS_CODES[status] && status < 500;
-  if (opts) merge(err, opts);
-  throw err;
-}
-
-function merge (a, b) {
-  for (var p in b) {
-    if (b.hasOwnProperty(p)) a[p] = b[p];
-  }
+  throw createError(status, msg, opts);
 }
