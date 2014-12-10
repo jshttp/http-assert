558822fa66aabd0a603ef9329e22c7e514d8d9db
Stephen Mathieson <smath23@gmail.com>
2014-12-10 09:59:34 -0500

    index: Add equality methods (equal, deepEqual, etc.)
    Closes #5.

--- test/test.js
+++ test/test.js
@@ -66,3 +66,117 @@ describe('assert()', function () {
     ok(!err.expose);
   });
 })
+
+describe('assert.equal()', function() {
+  it('should throw when things aren\'t equal', function(){
+    var err;
+    try {
+      assert.equal('a', 'b', 401, 'fail');
+    } catch (e) {
+      err = e;
+    }
+
+    ok(err);
+    ok(err.status == 401);
+    ok(err.message == 'fail');
+  })
+
+  it('should not throw when things are equal', function() {
+    assert.equal(1, '1', 401, 'fail');
+  })
+})
+
+describe('assert.notEqual()', function() {
+  it('should throw when things are equal', function(){
+    var err;
+    try {
+      assert.notEqual('a', 'a', 401, 'fail');
+    } catch (e) {
+      err = e;
+    }
+
+    ok(err);
+    ok(err.status == 401);
+    ok(err.message == 'fail');
+  })
+
+  it('should not throw when things aren\'t equal', function() {
+    assert.notEqual(1, 11, 401, 'fail');
+  })
+})
+
+describe('assert.strictEqual()', function() {
+  it('should throw when things aren\'t equal', function(){
+    var err;
+    try {
+      assert.strictEqual(1, '1', 401, 'fail');
+    } catch (e) {
+      err = e;
+    }
+
+    ok(err);
+    ok(err.status == 401);
+    ok(err.message == 'fail');
+  })
+
+  it('should not throw when things are equal', function() {
+    assert.strictEqual(1, 1, 401, 'fail');
+  })
+})
+
+describe('assert.notStrictEqual()', function() {
+  it('should throw when things are equal', function(){
+    var err;
+    try {
+      assert.notStrictEqual(1, 1, 401, 'fail');
+    } catch (e) {
+      err = e;
+    }
+
+    ok(err);
+    ok(err.status == 401);
+    ok(err.message == 'fail');
+  })
+
+  it('should not throw when things aren\'t equal', function() {
+    assert.notStrictEqual(1, '1', 401, 'fail');
+  })
+})
+
+describe('assert.deepEqual()', function() {
+  it('should throw when things aren\'t deeply equal', function(){
+    var err;
+    try {
+      assert.deepEqual({ a: 'a' }, { b: 'b' }, 401, 'fail');
+    } catch (e) {
+      err = e;
+    }
+
+    ok(err);
+    ok(err.status == 401);
+    ok(err.message == 'fail');
+  });
+
+  it('should not throw when things are deeply equal', function() {
+    assert.deepEqual({ a: 'a' }, { a: 'a' }, 401, 'fail');
+  })
+})
+
+describe('assert.notDeepEqual()', function() {
+  it('should throw when things aren\'t deeply equal', function(){
+    var err;
+    try {
+      assert.notDeepEqual({ a: 'a' }, { a: 'a' }, 401, 'fail');
+    } catch (e) {
+      err = e;
+    }
+
+    ok(err);
+    ok(err.status == 401);
+    ok(err.message == 'fail');
+  });
+
+  it('should not throw when things are deeply equal', function() {
+    assert.notDeepEqual({ a: 'a' }, { b: 'b' }, 401, 'fail');
+  })
+})
